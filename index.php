447c2b23edaf9a249c18ba1b3de14f0bdcae6cc6
Oluchy97 <140068141+Oluchy97@users.noreply.github.com>
2023-08-14 06:45:11 -0700

    klk

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+// fghjkjhgfghjxc
+
+// $name = 'oluchy arinze';
+// print("Welcome ".$name." rtyui". "srdtfyhg");
+// $arr = [
+//     'name'=>'oluchy'
+// ];
+// print($arr['name']);
+
+// $fruits = [
+//     'mango', 'apple', 'orange'
+// ];
+// print_r($fruits);
+
+// $age = 20;
+// if ($age < 10){
+//     print('way to go');
+// }elseif($age < 18){
+//     print('no way');
+// }else{
+//     print('Perfector');
+// }
+
+// SWITCH STATEMENT ON PHP is used to select on of the many blocks of code to be executed,m 
+
+// $Fav_Fruit = "apple";
+
+// switch ($Fav_Fruit) {
+//     case "orange": 
+//         echo "fav fruit is orange";
+//     break; 
+//     case "banana":
+//         echo 'fave fruit is banana';
+//     break;   
+//     case 'apple':
+//         echo 'fave fruit is apple';
+//     break;
+// }
+
+
+// FUNCTIONS
+
+// function add($orange, $apple){
+//     return $orange+$apple;
+// }
+// $res = add(10,15);
+// print($res);
+
+// OBJECT ORIENTED PROGRAMMING
+ 
+    // var_dump($_GET);
+
+//     $username ='root';
+//     $password ='';
+//     $database = 'login';
+//     $host = 'localhost';
+
+//     $conn = new mysqli($host, $username, $password, $database);
+//     $conn  ->query("INSERT INTO `user_data`
+//     (`id`, `name`, `username`, `email`, `password`)
+//      VALUES 
+//      (null,'Arinze','Arinzey1','arinze@gmail.com','9876543g2h1jsh')");
+
+//      $conn ->query("INSERT INTO `user_data`
+//      (`id`, `name`, `username`, `email`, `password`) 
+//      VALUES 
+//      (null,'johnbosco','jbnze1','jbnze@gmail.com','081lynda1997')");
+    
+// ?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get">
+    <input type="text" name="username">
+    <button type="submit">Submit</button>
+    </form>
+</body>
+</html> -->
---
+++ register.php
@@ -0,0 +1,50 @@
+<?php
+// Check that the form was submitted
+$issubmitted =isset($_POST["username"]);
+if($issubmitted){
+    // validate input
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    // validate
+
+    // input to database
+    $conn = new mysqli("localhost", "root","", "login");
+    if($conn->connect_error){
+        exit("Error Connecting To DB");
+    }
+
+    // escape for hacks
+    $surname = $conn->escape_string($name);
+    $surname = $conn->escape_string($username);
+    $email = $conn->escape_string($email);
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // RUN QUERY
+    $query = "INSERT INTO `user_data` (`name`, `username`, `email`, `password`) VALUES 
+    ('$name', '$username', '$email', '$password')";
+    $conn->query($query);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+   
+    <title>My first register php </title>
+
+</head>
+<body>
+    
+    <form method="post">
+        <input type="text" name="name" id="username" placeholder="name">
+        <input type="text" name="username" id="username" placeholder="username">
+        <input type="email" name="email" id="email" placeholder="email">
+        <input type="password" name="password" id="password" placeholder="password">
+        <button>submit</button>
+    </form>
+
+</body>
+</html>
